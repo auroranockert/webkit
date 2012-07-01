9b0b746638b00ad1c21df7336d197f06563a652d
Jens Nockert <jens@aventine.se>
2012-07-01 16:31:02 +0200

    Some reorganization in preparation for the Typed Array drop

--- Source/JavaScriptCore/runtime/TypedArray.h
+++ Source/JavaScriptCore/runtime/TypedArray.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012 Jens Nockert (jens@ofmlabs.org)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -10,17 +11,17 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef TypedArray_h
@@ -55,92 +56,15 @@ public:
         return ptr;
     }
 
-    static bool getOwnPropertySlot(JSCell* cell, ExecState* exec, PropertyName propertyName, PropertySlot& slot)
-    {
-        Current* thisObject = jsCast<Current*>(cell);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-        unsigned index = propertyName.asIndex();
-
-        if (index < thisObject->m_storageLength) {
-            ASSERT(index != PropertyName::NotAnIndex);
-            slot.setValue(thisObject->getByIndex(exec, index));
-            return true;
-        }
-
-        return Base::getOwnPropertySlot(cell, exec, propertyName, slot);
-    }
-
-    static bool getOwnPropertyDescriptor(JSObject* object, ExecState* exec, PropertyName propertyName, PropertyDescriptor& descriptor)
-    {
-        Current* thisObject = jsCast<Current*>(object);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-        unsigned index = propertyName.asIndex();
-
-        if (index < thisObject->m_storageLength) {
-            ASSERT(index != PropertyName::NotAnIndex);
-            descriptor.setDescriptor(thisObject->getByIndex(exec, index), DontDelete);
-            return true;
-        }
-
-        return Base::getOwnPropertyDescriptor(object, exec, propertyName, descriptor);
-    }
-    
-    static bool getOwnPropertySlotByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, PropertySlot& slot)
-    {
-        Current* thisObject = jsCast<Current*>(cell);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-
-        if (propertyName < thisObject->m_storageLength) {
-            slot.setValue(thisObject->getByIndex(exec, propertyName));
-            return true;
-        }
-
-        return thisObject->methodTable()->getOwnPropertySlot(thisObject, exec, Identifier::from(exec, propertyName), slot);
-    }
-    
-    static void put(JSCell* cell, ExecState* exec, PropertyName propertyName, JSValue value, PutPropertySlot& slot)
-    {
-        Current* thisObject = jsCast<Current*>(cell);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-        unsigned index = propertyName.asIndex();
-
-        if (index != PropertyName::NotAnIndex) {
-            thisObject->indexSetter(exec, index, value);
-            return;
-        }
-
-        Base::put(thisObject, exec, propertyName, value, slot);
-    }
-    
-    static void putByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, JSValue value, bool)
-    {
-        Current* thisObject = jsCast<Current*>(cell);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-        thisObject->indexSetter(exec, propertyName, value);
-    }
-
-    static const ClassInfo s_info; // TODO: Implement
+    static const ClassInfo s_info;
 
     static Structure* createStructure(JSGlobalData& globalData, JSGlobalObject* globalObject, JSValue prototype)
     {
         return JSC::Structure::create(globalData, globalObject, prototype, TypeInfo(JSC::ObjectType, StructureFlags), &s_info);
     }
 
-    static void getOwnPropertyNames(JSObject* object, ExecState* exec, PropertyNameArray& propertyNames, EnumerationMode mode)
-    {
-        Current* thisObject = jsCast<Current*>(object);
-        ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
-
-        for (unsigned i = 0; i < thisObject->m_storageLength; ++i) {
-            propertyNames.add(Identifier::from(exec, i));
-        }
-
-        Base::getOwnPropertyNames(thisObject, exec, propertyNames, mode);
-    }
-
-    static JSValue getConstructor(ExecState*, JSC::JSGlobalObject*);
-
     // TODO: static const JSC::TypedArrayType TypedArrayStorageType = JSC::TypedArray##name;
+
     uint32_t m_storageLength;
 
     T* m_storage;
@@ -148,22 +72,7 @@ public:
     RefPtr<I> m_impl;
 
 protected:
-    TypedArray<T, I>(Structure* structure, JSGlobalObject* globalObject, PassRefPtr<I> impl)
-        : Base(globalObject->globalData(), structure)
-        , m_impl(impl)
-    {    
-    }
-
-    void finishCreation(JSC::JSGlobalData& globalData)
-    {
-        Base::finishCreation(globalData);
-        TypedArrayDescriptor descriptor(&Current::s_info, OBJECT_OFFSETOF(Current, m_storage), OBJECT_OFFSETOF(Current, m_storageLength));
-        globalData.registerTypedArrayDescriptor(m_impl.get(), descriptor);
-        m_storage = m_impl->data();
-        m_storageLength = m_impl->length();
-        putDirect(globalData, globalData.propertyNames->length, jsNumber(m_storageLength), DontDelete | ReadOnly | DontEnum);
-        ASSERT(inherits(&s_info));
-    }
+    void finishCreation(JSC::JSGlobalData& globalData);
 
     static const unsigned StructureFlags = JSC::OverridesGetPropertyNames | JSC::OverridesGetOwnPropertySlot | Base::StructureFlags;
 
@@ -184,7 +93,117 @@ protected:
     {
         m_impl->set(index, value.toNumber(exec));
     }
+private:
+    TypedArray<T, I>(Structure* structure, JSGlobalObject* globalObject, PassRefPtr<I> impl);
+    
+    static bool getOwnPropertySlot(JSCell* cell, ExecState* exec, PropertyName propertyName, PropertySlot& slot);
+    static bool getOwnPropertySlotByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, PropertySlot& slot);
+    static bool getOwnPropertyDescriptor(JSObject* object, ExecState* exec, PropertyName propertyName, PropertyDescriptor& descriptor);
+    
+    static void getOwnPropertyNames(JSObject* object, ExecState* exec, PropertyNameArray& propertyNames, EnumerationMode mode);
+    
+    static void put(JSCell* cell, ExecState* exec, PropertyName propertyName, JSValue value, PutPropertySlot& slot);
+    static void putByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, JSValue value, bool);
 };
+
+template<typename T, typename I>
+TypedArray<T, I>::TypedArray(Structure* structure, JSGlobalObject* globalObject, PassRefPtr<I> impl) : Base(globalObject->globalData(), structure), m_impl(impl)
+{
+}
+
+template<typename T, typename I>
+void TypedArray<T, I>::finishCreation(JSC::JSGlobalData& globalData)
+{
+    Base::finishCreation(globalData);
+    TypedArrayDescriptor descriptor(&Current::s_info, OBJECT_OFFSETOF(Current, m_storage), OBJECT_OFFSETOF(Current, m_storageLength));
+    globalData.registerTypedArrayDescriptor(m_impl.get(), descriptor);
+    m_storage = m_impl->data();
+    m_storageLength = m_impl->length();
+    putDirect(globalData, globalData.propertyNames->length, jsNumber(m_storageLength), DontDelete | ReadOnly | DontEnum);
+    ASSERT(inherits(&s_info));
+}
+
+template<typename T, typename I>
+bool TypedArray<T, I>::getOwnPropertySlot(JSCell* cell, ExecState* exec, PropertyName propertyName, PropertySlot& slot)
+{
+    Current* thisObject = jsCast<Current*>(cell);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+    unsigned index = propertyName.asIndex();
+
+    if (index < thisObject->m_storageLength) {
+        ASSERT(index != PropertyName::NotAnIndex);
+        slot.setValue(thisObject->getByIndex(exec, index));
+        return true;
+    }
+
+    return Base::getOwnPropertySlot(cell, exec, propertyName, slot);
+}
+
+template<typename T, typename I>
+bool TypedArray<T, I>::getOwnPropertySlotByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, PropertySlot& slot)
+{
+    Current* thisObject = jsCast<Current*>(cell);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+
+    if (propertyName < thisObject->m_storageLength) {
+        slot.setValue(thisObject->getByIndex(exec, propertyName));
+        return true;
+    }
+
+    return thisObject->methodTable()->getOwnPropertySlot(thisObject, exec, Identifier::from(exec, propertyName), slot);
+}
+
+template<typename T, typename I>
+bool TypedArray<T, I>::getOwnPropertyDescriptor(JSObject* object, ExecState* exec, PropertyName propertyName, PropertyDescriptor& descriptor)
+{
+    Current* thisObject = jsCast<Current*>(object);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+    unsigned index = propertyName.asIndex();
+
+    if (index < thisObject->m_storageLength) {
+        ASSERT(index != PropertyName::NotAnIndex);
+        descriptor.setDescriptor(thisObject->getByIndex(exec, index), DontDelete);
+        return true;
+    }
+
+    return Base::getOwnPropertyDescriptor(object, exec, propertyName, descriptor);
+}
+
+template<typename T, typename I>
+void TypedArray<T, I>::getOwnPropertyNames(JSObject* object, ExecState* exec, PropertyNameArray& propertyNames, EnumerationMode mode)
+{
+    Current* thisObject = jsCast<Current*>(object);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+
+    for (unsigned i = 0; i < thisObject->m_storageLength; ++i) {
+        propertyNames.add(Identifier::from(exec, i));
+    }
+
+    Base::getOwnPropertyNames(thisObject, exec, propertyNames, mode);
+}
+
+template<typename T, typename I>
+void TypedArray<T, I>::put(JSCell* cell, ExecState* exec, PropertyName propertyName, JSValue value, PutPropertySlot& slot)
+{
+    Current* thisObject = jsCast<Current*>(cell);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+    unsigned index = propertyName.asIndex();
+
+    if (index != PropertyName::NotAnIndex) {
+        thisObject->indexSetter(exec, index, value);
+        return;
+    }
+
+    Base::put(thisObject, exec, propertyName, value, slot);
+}
+
+template<typename T, typename I>
+void TypedArray<T, I>::putByIndex(JSCell* cell, ExecState* exec, unsigned propertyName, JSValue value, bool)
+{
+    Current* thisObject = jsCast<Current*>(cell);
+    ASSERT_GC_OBJECT_INHERITS(thisObject, &s_info);
+    thisObject->indexSetter(exec, propertyName, value);
+}
 
 typedef TypedArray<uint8_t, Uint8Array> JSUint8Array;
 typedef TypedArray<uint8_t, Uint8ClampedArray> JSUint8ClampedArray;
